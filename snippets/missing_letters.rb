031198cb8c0a2545002e923c2337644b1738219a
Victor Nwaiwu <victor.nwaiwu@andela.com>
2018-09-11 22:54:14 +0100

    fix: remove unused variable in method

--- snippets/missing_letters.rb
+++ snippets/missing_letters.rb
@@ -1,5 +1,5 @@
 def missing_letters(given_letters)
   all_letters = given_letters.min.upto(given_letters.max).to_a
 
-  missing_letters = all_letters - given_letters
+  all_letters - given_letters
 end
