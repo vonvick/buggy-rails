b1c768bfd0936ec180c746871a5e264bd0363ec6
Victor Nwaiwu <victor.nwaiwu@andela.com>
2018-09-12 13:54:04 +0100

    fix: write more tests to take care of edge cases in count_words method

--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 require_relative '../snippets/count_words'
 
-describe '#count_words' do
-  it 'should return correct results' do
+describe "#count_words" do
+  it "should return correct results" do
     expected_result = {
       This: 1,
       is: 2,
@@ -12,5 +12,15 @@ describe '#count_words' do
       sweet: 1
     }
     expect(count_words("This is chocolate and chocolate is very sweet")).to eq expected_result
+  end
+
+  it "differentiates between case-sensitive words" do
+    result = count_words("This is chocolate and Chocolate is very sweet")
+
+    expect(result[:chocolate]).to eq(1)
+  end
+
+  it "returns an empty hash if no word is passed in" do
+    expect(count_words("")).to eq({})
   end
 end
