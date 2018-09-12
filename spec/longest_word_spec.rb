3e6029a4ff78af3122922b25ce7c6a144fccb4aa
Victor Nwaiwu <victor.nwaiwu@andela.com>
2018-09-12 15:59:18 +0100

    fix: add more tests to handle edge cases for longest_word method

--- spec/longest_word_spec.rb
+++ spec/longest_word_spec.rb
@@ -2,7 +2,23 @@ require 'spec_helper'
 require_relative '../snippets/longest_word'
 
 describe '#longest_word' do
-  it 'should return correct results' do
-    expect(longest_word("Here is the longest word")).to eq "longest"
+  it "should return correct results" do
+    expect(longest_word("Here is the longest word")).to eq ["longest"]
+  end
+
+  it "should return list of words if more than word are the longest" do
+    expect(
+      longest_word(
+        "Some words are longer than others"
+      )
+    ).to eq(%w(longer others))
+  end
+
+  it "should return list of words if more than word are the longest" do
+    expect(
+        longest_word(
+            "Some words are others than longer"
+        )
+    ).to eq(%w(longer others))
   end
 end
