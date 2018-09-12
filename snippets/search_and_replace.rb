3eb44c12845138fa78563d8f88cc4f4ff0008ba3
Victor Nwaiwu <victor.nwaiwu@andela.com>
2018-09-12 17:15:14 +0100

    fix: remove extra tests

--- snippets/search_and_replace.rb
+++ snippets/search_and_replace.rb
@@ -2,5 +2,5 @@ def search_and_replace(text, word, replace)
   text_collection = text.split
   word_index = text_collection.find_index(word)
   text_collection[word_index] = replace
-  text_collection.join('')
+  text_collection.join(' ')
 end
